9631b84d58276b3cb33a221bedac6c7276d0992e
Giulianna Bortone <70867985+Giuliannabortone@users.noreply.github.com>
2023-03-11 17:44:24 -0400

    Update Inicio.jsx

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -9,20 +9,22 @@ export default function Inicio() {
   },[]);
   console.log(peliculas);
   return (
-  
-  <React.Fragment>
-    <div className=''>
-      <img className='absolute z-0' src="src\assets\beyond-the-universe-4.jpg" alt="" />
-    <div className='relative z-5'>
-      <h1 className='font-bold text-blue-800 text-4xl text-center mt-8 flex justify-center grid '> Bienvenido a tu app de peliculas </h1>
-    </div>
 
+    <React.Fragment>
       
+      <div className=''>
+        <img className='absolute z-0' src="src\assets\beyond-the-universe-4.jpg" alt="" />
+        <div className='relative z-5'>
+        <h1 className='font-bold text-blue-800 text-4xl text-center mt-8 flex justify-center grid '> Bienvenido a tu app de peliculas </h1>
+      </div>
+
+      </div>
       
 
 
 
     </React.Fragment>
+    
     
     
     
