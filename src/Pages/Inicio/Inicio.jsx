42a1eff02de1a33f0b842a88c33d7d3c7ef8ffea
Giulianna Bortone <70867985+Giuliannabortone@users.noreply.github.com>
2023-03-11 17:12:10 -0400

    commit tailwwind arreglado

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function Inicio() {
   return (
-    <div>Inicio</div>
+
+    <React.Fragment>
+      
+      <div className=''>
+        <img className='absolute z-0' src="" alt="" />
+        <h1 className='font-bold text-blue-800 text-4xl mt-3 text-center mt-8'> Bienvenido a tu app de peliculas </h1>
+      </div>
+      
+      <div>
+
+
+
+
+      </div>
+
+
+
+    </React.Fragment>
+    
+    
+    
+
+
   )
 }
