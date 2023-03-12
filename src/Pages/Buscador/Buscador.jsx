f0565bff5bb831a05196d27d85fd46601e59c86d
Isabela Espinoza <isabela.espinoza@correo.unimet.edu.ve>
2023-03-12 08:17:11 -0400

    cajita del buscador lista
    cajita del buscador lista

--- src/Pages/Buscador/Buscador.jsx
+++ src/Pages/Buscador/Buscador.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 
 export default function Buscador() {
   return (
-    <div>Buscador</div>
+    
+
+  <form className='ml-16 mr-16'>
+    <br></br> 
+     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-blue-700">
+          <img 
+          className="w-8 h-8 mr-2" 
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" 
+          alt="logo"
+          />
+          Neiflis    
+      </a>
+    <div className='className="flex items-center mb-6 text-xl font-semibold text-gray-900'>
+      <h1 className=''>Buscador de películas</h1>
+       
+    </div> 
+    <label for="default-search" class="mb-2 text-sm font-medium sr-only ">Buscar</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg 
+            aria-hidden="true" 
+            class="w-5 h-5 " 
+            fill="none" 
+            stroke="currentColor" 
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg">
+              <path 
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              stroke-width="2" 
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+              </path>
+                
+            </svg>
+        </div>
+        <input 
+        type="search" 
+        id="default-search" 
+        class="block w-full p-4 pl-10 text-sm  border  rounded-lg  focus:ring-blue-500 focus:border-blue-500  dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        placeholder="Busca tu película favorita..." required/>
+        <button 
+        type="submit" 
+        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+    </div>
+  </form>
+
   )
 }
