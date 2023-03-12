3742862f0eb948f09a0cf4c61b6427efdb4fa2be
Giulianna Bortone <70867985+Giuliannabortone@users.noreply.github.com>
2023-03-12 14:45:48 -0400

    commit inicio con logo
    inicio con logo

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -14,9 +14,17 @@ export default function Inicio() {
   return (
 
     <React.Fragment>
-      
+
+      <a href="#" className=" mt-6 ml-6 flex items-center mb-6 text-2xl font-semibold text-blue-700 dark:text-blue-700">
+          <img 
+          className="w-8 h-8 mr-2" 
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" 
+          alt="logo"
+          />
+          Neiflis    
+      </a>
       <div className='relative z-5 bg-white mb-2'>
-        <h1 className='font-bold text-blue-800 text-4xl text-center mt-8 flex justify-center grid '> Bienvenido a tu app de peliculas </h1>
+        <h1 className='font-bold text-blue-800 text-4xl text-center mt-8 flex justify-center grid '> Bienvenido a tu app de películas </h1>
       </div>
       <img className='h-auto max-w-lg rounded-lg mx-auto' src="src\assets\beyond-the-universe-4.jpg" alt="" />
       <br></br>
