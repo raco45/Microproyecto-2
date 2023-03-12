6301ba0361d17874ef7c12efe37db113b2d6fa12
Giulianna Bortone <70867985+Giuliannabortone@users.noreply.github.com>
2023-03-12 13:17:08 -0400

    commit inicion casi listo, falta imagen y paginacion

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,4 +1,3 @@
-
 import { usePeliculas } from '../../Hooks/usePeliculas';
 import { useEffect } from 'react';
 import React from 'react'
@@ -6,27 +5,37 @@ import Card from '../../Components/Cards/card';
 
 export default function Inicio() {
   const {peliculas, getPeliculas}= usePeliculas();
+  let pagination = 1
 
   useEffect(()=>{
-    getPeliculas(2);
+    getPeliculas(pagination);
   },[]);
   console.log(peliculas);
   return (
 
     <React.Fragment>
       
-      <div className=''>
-        <img className='absolute z-0' src="src\assets\beyond-the-universe-4.jpg" alt="" />
-        <div className='relative z-5 bg-white mb-2'>
+      <div className='relative z-5 bg-white mb-2'>
         <h1 className='font-bold text-blue-800 text-4xl text-center mt-8 flex justify-center grid '> Bienvenido a tu app de peliculas </h1>
       </div>
-      <div className=''>
+      <img className='h-auto max-w-lg rounded-lg mx-auto' src="src\assets\beyond-the-universe-4.jpg" alt="" />
+      <br></br>
+      <div className='bg-blue-700 h-16'></div>
+      <div className='h-8 grid grid-cols-2 gap-4 content-start'>
        {peliculas.map((pelicula) => (
-              <Card pelicula={pelicula} />
+          <div className=''>
+            <Card pelicula={pelicula} />
+          </div>  
             ))
           }
         </div>
-      </div>
+
+        <div>
+          
+
+
+        </div>
+  
     </React.Fragment>
     
     
